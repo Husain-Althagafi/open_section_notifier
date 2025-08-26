9572df08c0fcf65f6c833ca9d3e77c05b9965fe0
Husain-Althagafi <husain.a.althagafi@okaz.com>
2025-08-27 00:09:35 +0300

    incorporated a telegram bot and added logic that checks if the section you are looking for is open. need to add the actual inputting of the CRN. plan is that u send a command to the tele bot and tell it the CRN and department of the course u want to track then it will track based on that

--- course_offering.py
+++ course_offering.py
@@ -1,7 +1,18 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def send_request():
+
+    """
+    Send a GET request to the course-offering.com API to retrieve course data.
+
+    Returns the JSON response containing course information for the specified department and CRN.
+
+    """
 
     # Configuration for the request
     headers = {
@@ -37,14 +48,72 @@ def send_request():
     #     if course['crn'] == CRN:
     #         print('COURSE FOUND BY CRN:', CRN)
     #         print(json.dumps(course, indent=4))
-
+    i = 0
     for course in response.json()['data']:
+        if i < 5:
+            is_open(course)
+            i+=1
+
         if course['name'] == 'ICS433' and course['sections'][0]['schedule']['time']['start'] == '1000':
             print('COURSE FOUND')
             print(json.dumps(course, indent=4))
-            
+            is_open(course)
+
+    
+
+
+def send_telegram_message(message_text):
+
+    """
+    Send a message via Telegram bot. Uses github secrets for bot token and chat ID.
+    
+    """
+
+    bot_token = os.environ.get("TELEGRAM_BOT_TOKEN")
+    chat_id = os.environ.get("TELEGRAM_CHAT_ID")
+
+    if not bot_token or not chat_id:
+        print("Error with telegram bot")
+        return
+    
+    api_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+
+    escaped_text = message_text.replace("-", "\\-").replace(".", "\\.").replace("!", "\\!")
+
+    payload = {
+        'chat_id': chat_id,
+        'text': escaped_text,
+        'parse_mode': 'MarkdownV2'
+    }
+
+    try:
+        response = requests.post(api_url, json=payload)
+        response.raise_for_status()     
+        print("Telegram notification sent successfully")
+    except requests.exceptions.RequestException as e:
+            print(f"Error sending Telegram message: {e}")
+
+
+def is_open(course):
+
+    """
+    Check if a course section is open based on its if there are available seats.
+
+    """
+
+    max_seats = course['capacity']['seats']['maximum']
+    taken_seats = course['capacity']['seats']['taken']
+    available_seats = max_seats - taken_seats
+    
+    if available_seats > 0:
+        print("SECTION HAS OPEN SEATS")
+        message = f"Section {course['crn']} of course: {course['name']} is open with {available_seats} available seats!"
+        send_telegram_message(message)
+    else:
+        print("SECTION IS FULL")
+    
+      
 
 if __name__ == "__main__":
     print("Retreiving course data from course-offering.com")
-
     send_request()
